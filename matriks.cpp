fde5dd344913d0c06727d88187f6d9fcaacc4fe8
faishalnafi <faishalnafi50@gmail.com>
2024-03-25 01:19:10 +0700

    matriks eliminasi gaus

---
+++ matriks.cpp
@@ -0,0 +1,32 @@
+/*
+Created By FaishalNafi'
+C++ programming class
+Matriks Eliminasi Gaus
+*/
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
+#define SIZE 10
+using namespace std;
+
+int main() {
+    float a[SIZE][SIZE], x[SIZE], ratio;
+    int i,j,k,n;
+
+    //Presisi Nilai;
+    cout << setprecision(3) << fixed;
+
+    /*Input nilai matriks*/
+    /*Definisi nilai matriks*/
+    cout << "Masukkan ukuran matriks: ";
+    cin >> n;
+
+    /*Input value matriks*/
+    cout << "Masukkan nilai angka tiap elemen matriks: ";
+    
+    return 0;
+}

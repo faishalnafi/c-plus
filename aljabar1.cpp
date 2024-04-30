d43b983d76cd4895f450e2cf8fbc44ea5f01d74e
faishalnafi <faishalnafi50@gmail.com>
2024-04-30 23:55:49 +0700

    update: aljabar uts v3

--- aljabar1.cpp
+++ aljabar1.cpp
@@ -7,25 +7,31 @@ using namespace std;
 
 int main (){
     float a[SIZE][SIZE], x[SIZE], ratio;int i, j, k, n;
+
     //Nilai pengatur Presisi
     cout << setprecision(3) << fixed;
+
     /*Input Matriks*///Menentukan Ukuran Matriks
     cout << "Masukkan ukuran matriks: ";cin >> n;
+
     //Memasukkan nilai angka matriks
-    cout << "Masukkan nilai matriks Setiap elemen : " << endl;for (i=1;i<=n;i++)
-    {for (j=1;j<=n+1;j++){
-    cout << "a[" << i << "][" << j << "] =";cin >> a[i][j];
-    }}
-    //Penerapan Eliminasi Gaussfor (i=1;i<=n;i++)
-    {
-    if (a[i][i] == 0.0){
-    cout << "Perhitungan Matriks Error";exit (0);
-    }for (j=i+1;j<=n;j++)
-    {
-    ratio = a[j][i] / a[i][i];
-    for (k=1;k<=n+1;k++){
-    a[j][k] = a[j][k]-ratio*a[i][k];}
+    cout << "Masukkan nilai matriks Setiap elemen : " << endl;
+    for (i=1;i<=n;i++) {
+        for (j=1;j<=n+1;j++) {
+            cout << "a[" << i << "][" << j << "] =";cin >> a[i][j];
+        }
     }
+
+    //Penerapan Eliminasi Gaussfor (i=1;i<=n;i++)
+    if (a[i][i] == 0.0){
+        cout << "Perhitungan Matriks Error";
+        exit (0);
+    }
+    for (j=i+1;j<=n;j++){
+        ratio = a[j][i] / a[i][i];
+        for (k=1;k<=n+1;k++) {
+            a[j][k] = a[j][k]-ratio*a[i][k];
+        }
     }
 
     //Menghitung nilai x y z dengan substitusi balikx[n] = a[n][n+1] / a[n][n];
